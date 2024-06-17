507b5a63a4bd8698331c70c29a8b0d2b40e66aec
Karmveer Singh <karmveer.nz@gmail.com>
2024-06-17 22:44:48 +1200

    Setup basic repo structure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen.tsx";
+import TestScreen from "./screens/TestScreen.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/"  element={<App />}>
+            <Route index={true} element={<HomeScreen />} />
+            <Route path={"/test"} element={<TestScreen />} />
+        </Route>
+    )
 )
+
+const root = document.getElementById("root") as HTMLElement;
+ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+            <RouterProvider router={router} />
+    </React.StrictMode>
+);
