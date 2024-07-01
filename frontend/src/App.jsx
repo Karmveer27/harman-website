5c086dd89cabd29992dacfe11c48485c1076f9e6
Karmveer Singh <karmveer.nz@gmail.com>
2024-07-02 01:07:33 +1200

    Started on Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css';
-import Nav from './components/Nav.jsx';
+import Nav from './components/Header/index.jsx';
 import Footer from './components/Footer.jsx';
 import { Outlet } from 'react-router-dom';
 
 function App() {
     return (
-        <div className="d-flex flex-column min-vh-100">
+        <div className="flex flex-col min-h-screen">
             <Nav />
-            <main className="flex">
+            <main className="flex-grow">
                     <Outlet />   
             </main>
             <Footer />
