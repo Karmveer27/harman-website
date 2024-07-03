0ead1277c8776b26a44e270512c192cc643db0e4
Karmveer Singh <karmveer.nz@gmail.com>
2024-07-04 00:40:33 +1200

    finished testimonials for small screens

--- frontend/src/components/Testimonials/index.jsx
+++ frontend/src/components/Testimonials/index.jsx
@@ -18,22 +18,41 @@ function Testimonials() {
     }
 
     return (
-        <div className="container py-5">
-            <h1>Testimonials</h1>
-            <Carousel interval={5000} wrap={true}  data-bs-theme="dark">
-                {chunkedData.map((chunk, chunkIndex) => (
-                    <Carousel.Item key={chunkIndex}>
-                        <Row className="g-5">
-                            {chunk.map((item, index) => (
-                                <Col key={index} lg={4}>
+        <>
+           
+            <div className="container py-5 d-none d-lg-block">
+                <h1>Testimonials</h1>
+                <Carousel interval={5000} wrap={true} data-bs-theme="dark">
+                    {chunkedData.map((chunk, chunkIndex) => (
+                        <Carousel.Item key={chunkIndex}>
+                            <Row className="g-5">
+                                {chunk.map((item, index) => (
+                                    <Col key={index} lg={4}>
+                                        <TestCard name={item.name} review={item.review} />
+                                    </Col>
+                                ))}
+                            </Row>
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+            </div>
+
+          
+            <div className="container py-5 d-lg-none">
+                <h1>Testimonials</h1>
+                <Carousel interval={5000} wrap={true} data-bs-theme="dark">
+                    {tempData.map((item, index) => (
+                        <Carousel.Item key={index}>
+                            <Row className="g-5 justify-content-center">
+                                <Col xs={12}>
                                     <TestCard name={item.name} review={item.review} />
                                 </Col>
-                            ))}
-                        </Row>
-                    </Carousel.Item>
-                ))}
-            </Carousel>
-        </div>
+                            </Row>
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+            </div>
+        </>
     );
 }
 
