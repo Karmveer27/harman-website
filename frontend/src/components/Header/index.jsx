20c5799a5758b55b86037e78f1adc1b72e3fd9c1
Karmveer Singh <karmveer.nz@gmail.com>
2024-07-05 17:58:54 +1200

    Added smooth scroll when on homescreen

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import './index.css';
+import { Link } from 'react-router-dom';
+import { Link as ScrollLink } from 'react-scroll';
+import { useState } from 'react';
+import { Navbar,Container,Nav } from 'react-bootstrap';
 
 function Header() {
     const [activeLink, setActiveLink] = useState('#about');
@@ -14,45 +13,25 @@ function Header() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary fixed-top">
             <Container fluid>
-                <Navbar.Brand href="/" className="ms-3">Harman Singh</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/" className="ms-3">Harman Singh</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mx-auto">
-                        <Nav.Link
-                            href="/"
-                            className={activeLink === '#about' ? 'active' : ''}
-                            onClick={() => handleSetActiveLink('#about')}
-                        >
+                        <Nav.Link as={Link} to="/#about" onClick={() => handleSetActiveLink('#about')}>
                             About Us
                         </Nav.Link>
-                        <Nav.Link
-                            href="#latest-work"
-                            className={activeLink === '#latest-work' ? 'active' : ''}
-                            onClick={() => handleSetActiveLink('#latest-work')}
-                        >
+                        <Nav.Link as={Link} to="/#work" onClick={() => handleSetActiveLink('#latest-work')}>
                             Our Latest Work
                         </Nav.Link>
-                        <Nav.Link
-                            href="#testimonials"
-                            className={activeLink === '#testimonials' ? 'active' : ''}
-                            onClick={() => handleSetActiveLink('#testimonials')}
-                        >
+                        <Nav.Link as={Link} to="/#testimonials" onClick={() => handleSetActiveLink('#testimonials')}>
                             Testimonials
                         </Nav.Link>
-                        <Nav.Link
-                            href="#properties"
-                            className={activeLink === '#properties' ? 'active' : ''}
-                            onClick={() => handleSetActiveLink('#properties')}
-                        >
+                        <Nav.Link as={Link} to="/#properties" onClick={() => handleSetActiveLink('#properties')}>
                             Properties
                         </Nav.Link>
                     </Nav>
                     <Nav className="ms-auto me-3">
-                        <Nav.Link
-                            href="/contact"
-                            className="nav-link-no-underline"
-                            onClick={() => handleSetActiveLink('#')}
-                        >
+                        <Nav.Link as={Link} to="/contact" className="nav-link-no-underline" onClick={() => handleSetActiveLink('#')}>
                             <button className="btn btn-warning btn-md">Contact us</button>
                         </Nav.Link>
                     </Nav>
