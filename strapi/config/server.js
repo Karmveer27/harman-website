14a6e946e06c4ca659ac44c76d9f9b0a6bbfd33a
Karmveer Singh <karmveer.nz@gmail.com>
2024-07-23 13:00:30 +1200

    Reinstalled a new strapi version compatible with node version

--- strapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'eba4aaab0c715defa41d5160b673b190'),
-    },
-  },
-});
--- strapi/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'adba2d86-262e-4a54-8c07-320aa75784c3'
-};
