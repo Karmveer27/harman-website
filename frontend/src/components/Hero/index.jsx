0deb911c286815a90060166b52f51dcdd316f379
Karmveer Singh <karmveer.nz@gmail.com>
2024-07-24 13:41:50 +1200

    Fixed link on hero page for conact us

--- frontend/src/components/Hero/index.jsx
+++ frontend/src/components/Hero/index.jsx
@@ -1,23 +1,22 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { Row, Col } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
 function Hero() {
-    const [homeImage,setHomeImage] = useState();
+    const [homeImage, setHomeImage] = useState('');
     
     useEffect(() => {
         fetch('http://localhost:1337/api/home-image/?populate=*')
-          .then((response) => response.json())
-          .then((data) => {
-            setHomeImage("http://localhost:1337" + data.data.attributes.image.data.attributes.url)
-            //console.log(data.data.attributes.image.data.attributes.url)
-          })
-          .catch((error) => console.error('Error fetching Image:', error));
-      }, []);
+            .then((response) => response.json())
+            .then((data) => {
+                setHomeImage("http://localhost:1337" + data.data.attributes.image.data.attributes.url);
+                // console.log(data.data.attributes.image.data.attributes.url);
+            })
+            .catch((error) => console.error('Error fetching Image:', error));
+    }, []);
 
-      
-      
     return (
         <Row className="align-items-center min-vh-100 hero-padding">
             <Col lg={6} md={12} className="mb-4 mb-lg-0">
@@ -26,7 +25,7 @@ function Hero() {
                 Harman Singh, an enthusiastic and devoted real estate professional, has joined the industry with a heartfelt mission: to assist individuals in discovering their dream homes and achieving their real estate aspirations in Auckland. 
                 </p>
                 <div className="d-flex justify-content-start">
-                    <button className="btn btn-warning btn-lg">Contact us</button>
+                    <Link to="/contact" className="btn btn-warning btn-lg">Contact us</Link>
                 </div>
             </Col>
             <Col lg={6} md={12} className="text-center hero-work-card">
